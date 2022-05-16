5055af629727c7bfdf0f8ee8e8484d0b18729272
HeynerPat <72895524+HeynerPat@users.noreply.github.com>
2022-05-16 13:47:16 -0500

    Reaccionar cuando recibimos una notificacion

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,22 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    //Context!
+    PushNotificationService.messagesStream.listen((message) {
+      print('MyApp: $message');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/src/services/push_notifications_service.dart
+++ lib/src/services/push_notifications_service.dart
@@ -1,23 +1,35 @@
 // SHA1: 28:46:E0:08:32:D4:1E:76:02:B4:A2:FB:E8:81:BA:1E:8E:45:BA:EC
 //TOKEN: fwsmDLV0Tu-hNWQG4fLnb9:APA91bG-iBWlNafZdxs403tPoJ3L4UuuXoRcu426OlmiJmA-iSyPlYd8qsWwV3Ttr0jc_tyl6nSz3C_QPzdoiPu2xBVDV-GPJv2zJPVRbGThvQL9rdfU38kgX60r7c_tc6Hllj5sJ3uL
 
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
+  static StreamController<String> _messageStream =
+      new StreamController.broadcast();
+
+  static Stream<String> get messagesStream => _messageStream.stream;
 
   static Future _backgroundHandler(RemoteMessage message) async {
-    print('onBackground Handler ${message.messageId}');
+    // print('onBackground Handler ${message.messageId}');
+    print(message.data);
+    _messageStream.add(message.notification?.body ?? 'No title');
   }
 
   static Future _onMessageHandler(RemoteMessage message) async {
-    print('onMessage Handler ${message.messageId}');
+    //print('onMessage Handler ${message.messageId}');
+    print(message.data);
+    _messageStream.add(message.notification?.body ?? 'No title');
   }
 
   static Future _onMessageOpenApp(RemoteMessage message) async {
-    print('onMessageOpenApp Handler ${message.messageId}');
+    //print('onMessageOpenApp Handler ${message.messageId}');
+    print(message.data);
+    _messageStream.add(message.notification?.body ?? 'No title');
   }
 
   static Future initializeApp() async {
@@ -32,5 +44,9 @@ class PushNotificationService {
     FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenApp);
 
     //Local Notifications
+  }
+
+  static closeStreams() {
+    _messageStream.close();
   }
 }
